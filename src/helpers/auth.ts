7336862a80efcd9bf9321a819fd87cec89ce151e
Cem Gencer <cem.gencer@me.com>
2023-08-08 12:54:07 +0300

    focusing on the auth middleware

---
+++ src/helpers/auth.ts
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+
+const api_config = require("../config/api.js");
+
+const authVerifier = (req, res, next) => {
+
+//TODO: Implement authentication as User routes middleware...
+    const authHeader = req.headers.token;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, api_config.api.jwt_secret, (err, user) => {
+            if (err) res.status(401).json("Invalid token");
+            req.user = user;
+            next()
+        })
+    } else {
+        return res.status(401).json("Not authenticated");
+    }
+}
+
+/* check if the current user */
+const access = (req, res, next) => {
+    authVerifier(req, res, () => {
+        if (req.user.id === req.params.id) {
+            next()
+        } else {
+            res.status(403).json("Not allowed to perform this task");
+        }
+    })
+}
+
+module.exports = { authVerifier, accessVerifier };
