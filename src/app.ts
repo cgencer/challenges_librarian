2d5cf12b9c64f106b3beb8028c6df3701ac3d66f
Cem Gencer <cem.gencer@me.com>
2023-08-14 23:30:16 +0300

    runs fine

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
-import * as bodyParser from 'body-parser';
 import helmet from 'helmet';
 import * as methodOverride from 'method-override';
 import * as morgan from 'morgan';
@@ -8,7 +7,6 @@ import dbInit from './database/db.js';
 import { Sequelize } from 'sequelize';
 
 import { Environment, Config } from "./config/config.type";
-
 import { config } from './config/config.js';
 
 import { errorHandler } from "./helpers/error.mw.js";
@@ -21,22 +19,26 @@ export default class App {
 	protected app: express.Application;
 
 	constructor(){
+		console.log(':::-----------------------');
+		console.log('::: Environment is: ' + process.env.ENV);
+		console.log(':::-----------------------');
 		dbInit.init(config.db_uri);
 
 		this.app = express();
 
 		this.app.use(helmet());
 		this.app.use(cors());
-		this.app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }));
-		this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+		this.app.use(express.json({ limit: '50mb', type: 'application/json' }));
+		this.app.use(express.urlencoded());
 
 		this.app.use(errorHandler);
 		this.app.use(notFoundHandler);
 
 		this.app.use('/', routes);
 
-		this.app.listen(3000, () => {
-  			return console.log(`Express is listening at http://localhost:3000`);
+		this.app.listen(config.port, () => {
+  			return console.log(`::: the API is avail@ http://localhost:${config.port}`);
 		});
 	}
 }
+new App();
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,7 @@
-//import config from './app-dev.json' with { type: 'json' };
-
+// HACK: to import json
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const jsonFile: any = require('./app-dev.json');
 
+const jsonFile: any = require(`./app-${process.env.ENV}.json`);
 const config = Object.assign({}, jsonFile);
-
 export { config };
