3f74a403189842dbfc8cfb7b34c9eb83ecf1f829
Cem Gencer <cem.gencer@me.com>
2023-08-22 12:48:41 +0300

    env variables for nodeman need to be within its config file (nodemon.json)

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,14 @@
 // HACK: to import json
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
+import jsonFileConfigDev from './app-dev.json' assert { type: 'json' };
+import jsonFileConfigProd from './app-prod.json' assert { type: 'json' };
+import jsonFilePolicies from './policies.json' assert { type: 'json' };
 
 import { TEnvironment, IConfig } from "./config.type.js";
 
-//const jsonFileConfig: any = require(`./config/app-${process.env.NODE_ENV || 'dev'}.json`);
-let conFile: string = process.env.NODE_ENV || 'dev';
-
-import jsonFileConfigDev from './app-dev.json' assert { type: 'json' };
-import jsonFileConfigProd from './app-prod.json' assert { type: 'json' };
-
+const conFile: string = process.env.NODE_ENV || 'dev';
 const config: any = Object.assign({}, conFile == 'dev' ? jsonFileConfigDev : jsonFileConfigProd);
-
-// const jsonFilePolicies: any = require('./policies.json');
-import jsonFilePolicies from './policies.json' assert { type: 'json' };
 
 const policies: any = Object.assign({}, jsonFilePolicies);
 export { config, policies };
